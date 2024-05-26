80bcc931fd731373f6f559ae7b41ce566ef366d8
Radic Ivo <89729414+PROPHECY55@users.noreply.github.com>
2024-05-26 20:19:35 +0200

    Added Failing Test ToDo 1

---
+++ spec/string_calculator_spec.js
@@ -0,0 +1,6 @@
+describe('Electrons Around The Cores', function() {
+    it('Is the result 6 of the array [1, 2, 3, 4, 5]?', function() {
+
+        expect(calculateSum(array)).toEqual(6);
+    });
+});
