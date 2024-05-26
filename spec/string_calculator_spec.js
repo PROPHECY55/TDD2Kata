0dc38f45de0c468523cb627ab0548e83ba357870
Wendelin Windhager <wendelin.windhager@gmail.com>
2024-05-26 20:22:18 +0200

    Added Failing Test ToDo 2

--- spec/string_calculator_spec.js
+++ spec/string_calculator_spec.js
@@ -3,4 +3,9 @@ describe('Electrons Around The Corners', function(){
         const array = [1, 2, 3, 4, 5];
         expect(calculateSum(array)).toEqual(6);
     });
+
+	it('Is the result 4 of the array [2, 2, 3, 3]?', function(){
+
+        expect(calculateSum(array)).toEqual(4);
+    });
 });
